469ef2383ec29556f3ccc2c7de333fc6af470375
anji <anzi0204@gmail.com>
2024-07-18 01:18:31 +0900

    Skill

--- Assets/Script/skill/Skills/SpeedChargeSkill.cs
+++ Assets/Script/skill/Skills/SpeedChargeSkill.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class SpeedChargeSkill : MonoBehaviour, ISkill
 {
+    PlayerData playerData;
+
     [SerializeField] int skillCost;
     [SerializeField] Sprite skillIcon;
+
+    [SerializeField] float chargeRatio;
+    [SerializeField] float attackRatio;
+
+    public int skillLv; //このスキルレベル
+    [SerializeField] float chrgeLvRatio; //レベルによる変化比率定数
+    [SerializeField] float attackLvRatio;
 
     public Skill SkillData() => new Skill(3, "コスパコキュウ", "サッと　たまって　スッと　だす", skillCost, 1, skillIcon);
 
     ISkill.SkillType skillType = ISkill.SkillType.ChargeChange;
 
+    void Start()
+    {
+        playerData = GameManagerScript.instance.GetPlayerData();
+    }
+
     public void RunStartActionScene()
     {
-
+        playerData.ChargeRatio = chargeRatio + chrgeLvRatio * skillLv;
+        playerData.attackRatio = attackRatio + attackLvRatio * skillLv;
     }
 }
