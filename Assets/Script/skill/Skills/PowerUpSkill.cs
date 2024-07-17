691141fcffe94d39dc6d8689933839646d0d4b9b
anji <anzi0204@gmail.com>
2024-07-17 16:50:09 +0900

    gamemanager

--- Assets/Script/skill/Skills/PowerUpSkill.cs
+++ Assets/Script/skill/Skills/PowerUpSkill.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class PowerUpSkill : MonoBehaviour, ISkill
 {
+    PlayerData playerData;
+
     [SerializeField] int skillCost;
     [SerializeField] Sprite skillIcon;
 
     public Skill SkillData() => new Skill(1, "パワーアッピ", "すえるくうき　の　しつがあがって　パワーアップ", skillCost, 1, skillIcon);
 
     ISkill.SkillType skillType = ISkill.SkillType.StatesUp;
+
+    void Start()
+    {
+        playerData = GameManagerScript.instance.GetPlayerData();
+    }
 
     public void RunStartActionScene()
     {
